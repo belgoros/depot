07edfe1c1fe373a9f6a970825c95fd95699dcb78
belgoros <s.cambour@gmail.com>
2025-08-13 10:26:35 +0200

    Fix failing controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    login_as @user
   end
 
   test "should get index" do
@@ -17,7 +18,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email_address: @user.email_address, name: @user.name, password: "secret", password_confirmation: "secret" } }
+      post users_url, params: {
+        user: {
+          email_address: "sam@example.org",
+          name: "sam",
+          password: "secret",
+          password_confirmation: "secret"
+        }
+      }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +42,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email_address: @user.email_address, name: @user.name, password: "secret", password_confirmation: "secret" } }
+    patch user_url(@user), params: {
+      user: {
+        email_address: @user.email_address,
+        name: @user.name,
+        password: "secret",
+        password_confirmation: "secret"
+      }
+    }
     assert_redirected_to user_url(@user)
   end
 
